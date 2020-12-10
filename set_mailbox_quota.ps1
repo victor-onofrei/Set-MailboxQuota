de8161e21afb847d4953501a7bf90c7fa7bb8b54
victor-onofrei <39416712+victor-onofrei@users.noreply.github.com>
2020-12-10 15:44:35 +0200

    Update set_mailbox_quota.ps1

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -8,9 +8,9 @@ $default_RecoverableItemsQuota = "30GB"
 $default_RecoverableItemsWarningQuota = "20GB"
 
 foreach ($mailbox in $all_mailboxes) {
-    $mailbox_size = Get-MailboxStatistics -Identity $mailbox | select @{Name="TotalItemSizeinGB";`
-        Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}}`
-        | Select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
+    $mailbox_size = Get-MailboxStatistics -Identity $mailbox | select @{Name="TotalItemSizeinGB"; `
+    Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | `
+     Select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
     if ($mailbox_size -eq 0) {
         $desired_quota = 2GB 
     } elseif ($mailbox_size%1 -lt 0.5 -and $mailbox_size%1 -gt 0) {
@@ -27,8 +27,8 @@ foreach ($mailbox in $all_mailboxes) {
 
     if ((Get-Mailbox -Identity $mailbox).ArchiveStatus -ne "None") {
         $archive_size = Get-MailboxStatistics -Identity $mailbox -Archive | select @{Name="TotalItemSizeinGB"; `
-        Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}}`
-        | Select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
+        Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | `
+         Select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
         if ($archive_size%1 -lt 0.5 -and $archive_size%1 -gt 0) {
             $desired_quota = ([math]::Round($archive_size)+5.5)*([math]::pow(2,30))
         } else {
