b5db7fdf2f8b713fad70a2547813552b3f0cbcbf
victor-onofrei <39416712+victor-onofrei@users.noreply.github.com>
2020-12-10 13:22:17 +0200

    Update set_mailbox_quota.ps1

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -1,35 +1,35 @@
 $migration_list = @()
-$inputpath = "$env:homeshare\VDI-UserData\Download\generic\inputs\mailbox_list.csv"
-$allmailbox = Get-Content $inputpath
+$input_path = "$env:homeshare\VDI-UserData\Download\generic\inputs\mailbox_list.csv"
+$all_mailboxes = Get-Content $input_path
 
 $default_ProhibitSendReceiveQuota = "100GB"
 $default_RecoverableItemsQuota = "30GB"
 $default_RecoverableItemsWarningQuota = "20GB"
 
-foreach ($mailbox in $allmailbox) 
+foreach ($mailbox in $all_mailboxes) 
 {
-    $mailboxsize = Get-MailboxStatistics -Identity $mailbox | select @{name="TotalItemSizeinGB"; expression={[math]::Round( `
+    $mailbox_size = Get-MailboxStatistics -Identity $mailbox | select @{name="TotalItemSizeinGB"; expression={[math]::Round( `
     ($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
-    if ($mailboxsize -eq 0) {
+    if ($mailbox_size -eq 0) {
        $desired_quota = 2GB 
-    } elseif ($mailboxsize%1 -lt 0.5 -and $mailboxsize%1 -gt 0) {
-       $desired_quota = ([math]::Round($mailboxsize)+1.5)*([math]::pow(2,30))
+    } elseif ($mailbox_size%1 -lt 0.5 -and $mailbox_size%1 -gt 0) {
+       $desired_quota = ([math]::Round($mailbox_size)+1.5)*([math]::pow(2,30))
     }
     else { 
-        $desired_quota = ([math]::Round($mailboxsize)+1)*([math]::pow(2,30))
+        $desired_quota = ([math]::Round($mailbox_size)+1)*([math]::pow(2,30))
     }
     $moving_ProhibitSendQuota = $desired_quota
     $moving_IssueWarningQuota = [int64]($desired_quota*0.9)
     Set-mailbox $mailbox -UseDatabaseQuotaDefaults $false -ProhibitSendQuota $moving_ProhibitSendQuota -ProhibitSendReceiveQuota $default_ProhibitSendReceiveQuota -RecoverableItemsQuota $default_RecoverableItemsQuota -RecoverableItemsWarningQuota $default_RecoverableItemsWarningQuota -IssueWarningQuota $moving_IssueWarningQuota
 
     if ((Get-Mailbox -Identity $mailbox).ArchiveStatus -ne "None") {
-    $archivesize = Get-MailboxStatistics -Identity $mailbox -Archive | select @{name="TotalItemSizeinGB"; expression={[math]::Round( `
+    $archive_size = Get-MailboxStatistics -Identity $mailbox -Archive | select @{name="TotalItemSizeinGB"; expression={[math]::Round( `
     ($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | select TotalItemSizeinGB -ExpandProperty TotalItemSizeinGB
-    if ($archivesize%1 -lt 0.5 -and $archivesize%1 -gt 0) {
-       $desired_quota = ([math]::Round($archivesize)+5.5)*([math]::pow(2,30))
+    if ($archive_size%1 -lt 0.5 -and $archive_size%1 -gt 0) {
+       $desired_quota = ([math]::Round($archive_size)+5.5)*([math]::pow(2,30))
     }
     else {
-        $desired_quota = ([math]::Round($archivesize)+5)*([math]::pow(2,30))
+        $desired_quota = ([math]::Round($archive_size)+5)*([math]::pow(2,30))
     }
     $moving_ArchiveQuota = $desired_quota
     $moving_ArchiveWarningQuota = $desired_quota*0.9
