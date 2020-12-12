8440a40a862b4263af1d23fb8d6fcf81b6eb09d4
Iulian Onofrei <iulian.onofrei@yahoo.com>
2020-12-12 16:55:05 +0200

    Fix inconsistent spacing and indenting

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -7,35 +7,58 @@ $defaultRecoverableItemsQuota = "30GB"
 $defaultRecoverableItemsWarningQuota = "20GB"
 
 foreach ($mailbox in $allMailboxes) {
-    $mailboxSize = Get-MailboxStatistics -Identity $mailbox | select @{Name="TotalItemSizeInGB"; `
-    Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | `
-     Select "TotalItemSizeInGB" -ExpandProperty "TotalItemSizeInGB"
+    $mailboxSize =
+        Get-MailboxStatistics -Identity $mailbox | `
+        select @{
+            Name = "TotalItemSizeInGB"; `
+            Expression = {
+                [math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",", "") / 1GB), 2)
+            }
+        } | `
+        Select "TotalItemSizeInGB" -ExpandProperty "TotalItemSizeInGB"
+
     if ($mailboxSize -eq 0) {
         $desiredQuota = 2GB
-    } elseif ($mailboxSize%1 -lt 0.5 -and $mailboxSize%1 -gt 0) {
-        $desiredQuota = ([math]::Round($mailboxSize)+1.5)*[math]::pow(2,30)
+    } elseif ($mailboxSize % 1 -lt 0.5 -and $mailboxSize % 1 -gt 0) {
+        $desiredQuota = ([math]::Round($mailboxSize) + 1.5) * [math]::pow(2, 30)
     } else {
-        $desiredQuota = ([math]::Round($mailboxSize)+1)*[math]::pow(2,30)
+        $desiredQuota = ([math]::Round($mailboxSize) + 1) * [math]::pow(2, 30)
     }
+
     $movingProhibitSendQuota = $desiredQuota
-    $movingIssueWarningQuota = [int64]($desiredQuota*0.9)
-    Set-Mailbox $mailbox -UseDatabaseQuotaDefaults $false -ProhibitSendQuota $movingProhibitSendQuota `
-        -ProhibitSendReceiveQuota $defaultProhibitSendReceiveQuota -RecoverableItemsQuota `
-        $defaultRecoverableItemsQuota -RecoverableItemsWarningQuota $defaultRecoverableItemsWarningQuota `
+    $movingIssueWarningQuota = [int64]($desiredQuota * 0.9)
+
+    Set-Mailbox $mailbox `
+        -UseDatabaseQuotaDefaults $false `
+        -ProhibitSendQuota $movingProhibitSendQuota `
+        -ProhibitSendReceiveQuota $defaultProhibitSendReceiveQuota `
+        -RecoverableItemsQuota $defaultRecoverableItemsQuota `
+        -RecoverableItemsWarningQuota $defaultRecoverableItemsWarningQuota `
         -IssueWarningQuota $movingIssueWarningQuota
 
     if ((Get-Mailbox -Identity $mailbox).ArchiveStatus -ne "None") {
-        $archiveSize = Get-MailboxStatistics -Identity $mailbox -Archive | select @{Name="TotalItemSizeInGB"; `
-        Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | `
-         Select "TotalItemSizeInGB" -ExpandProperty "TotalItemSizeInGB"
-        if ($archiveSize%1 -lt 0.5 -and $archiveSize%1 -gt 0) {
-            $desiredQuota = ([math]::Round($archiveSize)+5.5)*[math]::pow(2,30)
+        $archiveSize =
+            Get-MailboxStatistics -Identity $mailbox -Archive | `
+            select @{
+                Name = "TotalItemSizeInGB"; `
+                Expression = {
+                    [math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",", "") / 1GB), 2)
+                }
+            } | `
+            Select "TotalItemSizeInGB" -ExpandProperty "TotalItemSizeInGB"
+
+        if ($archiveSize % 1 -lt 0.5 -and $archiveSize % 1 -gt 0) {
+            $desiredQuota = ([math]::Round($archiveSize) + 5.5) * [math]::pow(2, 30)
         } else {
-            $desiredQuota = ([math]::Round($archiveSize)+5)*[math]::pow(2,30)
+            $desiredQuota = ([math]::Round($archiveSize) + 5) * [math]::pow(2, 30)
         }
+
         $movingArchiveQuota = $desiredQuota
-        $movingArchiveWarningQuota = $desiredQuota*0.9
-        Set-Mailbox $mailbox -UseDatabaseQuotaDefaults $false -ArchiveQuota $movingArchiveQuota -ArchiveWarningQuota `
-                                                                                        $movingArchiveWarningQuota
+        $movingArchiveWarningQuota = $desiredQuota * 0.9
+
+        Set-Mailbox $mailbox `
+            -UseDatabaseQuotaDefaults $false `
+            -ArchiveQuota $movingArchiveQuota `
+            -ArchiveWarningQuota $movingArchiveWarningQuota
     }
 }
