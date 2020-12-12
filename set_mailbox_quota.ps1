feee344dabd53e19168d6b96312da5cb265a34ed
Iulian Onofrei <iulian.onofrei@yahoo.com>
2020-12-12 16:50:37 +0200

    Rename a variable

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -1,6 +1,6 @@
 $inputPath = "$env:homeshare\VDI-UserData\Download\generic\inputs\"
-$filename = "mailbox_list.csv"
-$allMailboxes = Get-Content $inputPath\$filename
+$fileName = "mailbox_list.csv"
+$allMailboxes = Get-Content $inputPath\$fileName
 
 $defaultProhibitSendReceiveQuota = "100GB"
 $defaultRecoverableItemsQuota = "30GB"
