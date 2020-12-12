56e285337501394f59e3fde9a5db6c8c81c0f3fe
Iulian Onofrei <iulian.onofrei@yahoo.com>
2020-12-12 16:55:01 +0200

    Remove superfluous parentheses

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -13,9 +13,9 @@ foreach ($mailbox in $allMailboxes) {
     if ($mailboxSize -eq 0) {
         $desiredQuota = 2GB
     } elseif ($mailboxSize%1 -lt 0.5 -and $mailboxSize%1 -gt 0) {
-        $desiredQuota = ([math]::Round($mailboxSize)+1.5)*([math]::pow(2,30))
+        $desiredQuota = ([math]::Round($mailboxSize)+1.5)*[math]::pow(2,30)
     } else {
-        $desiredQuota = ([math]::Round($mailboxSize)+1)*([math]::pow(2,30))
+        $desiredQuota = ([math]::Round($mailboxSize)+1)*[math]::pow(2,30)
     }
     $movingProhibitSendQuota = $desiredQuota
     $movingIssueWarningQuota = [int64]($desiredQuota*0.9)
@@ -29,9 +29,9 @@ foreach ($mailbox in $allMailboxes) {
         Expression={[math]::Round(($_.TotalItemSize.ToString().Split("(")[1].Split(" ")[0].Replace(",","")/1GB),2)}} | `
          Select "TotalItemSizeInGB" -ExpandProperty "TotalItemSizeInGB"
         if ($archiveSize%1 -lt 0.5 -and $archiveSize%1 -gt 0) {
-            $desiredQuota = ([math]::Round($archiveSize)+5.5)*([math]::pow(2,30))
+            $desiredQuota = ([math]::Round($archiveSize)+5.5)*[math]::pow(2,30)
         } else {
-            $desiredQuota = ([math]::Round($archiveSize)+5)*([math]::pow(2,30))
+            $desiredQuota = ([math]::Round($archiveSize)+5)*[math]::pow(2,30)
         }
         $movingArchiveQuota = $desiredQuota
         $movingArchiveWarningQuota = $desiredQuota*0.9
