7f9013f7a8ef9929719aee51171cee08567826b1
Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>
2020-12-13 19:48:28 +0200

    Refactor complicated quota computation logic

--- set_mailbox_quota.ps1
+++ set_mailbox_quota.ps1
@@ -8,6 +8,62 @@ $defaultProhibitSendReceiveQuota = "100GB"
 $defaultRecoverableItemsQuota = "30GB"
 $defaultRecoverableItemsWarningQuota = "20GB"
 
+$mailboxMinimumQuotaDifference = 1
+$mailboxMinimumQuota = 2
+$mailboxQuotaStep = 0.5
+
+$archiveMinimumQuotaDifference = 1
+$archiveMinimumQuota = 5
+$archiveQuotaStep = 5.0
+
+function Get-QuotaForSize {
+    <#
+        .SYNOPSIS
+            Compute and return a quota based on the input size based on specific
+            step, minimum quota and minimum difference between size and quota.
+
+        .EXAMPLE
+            Get-QuotaForSize
+                -size 1.2
+                -minimumDifference 1
+                -minimumQuota 2
+                -step 0.5
+
+            This returns 2.5.
+    #>
+    param(
+        [Double]$size,
+        [Double]$minimumDifference,
+        [Double]$minimumQuota,
+        [Double]$step
+    )
+
+    $upscaledQuotaStep = [math]::Ceiling($step) / $step
+
+    $upscaledSize = ($size + $minimumDifference) * $upscaledQuotaStep
+    $downscaledQuota = [math]::Ceiling($upscaledSize) / $upscaledQuotaStep
+
+    [math]::Max($downscaledQuota, $minimumQuota)
+}
+
+function Get-BytesFromGigaBytes {
+    <#
+        .SYNOPSIS
+            Compute and return a quota based on the input size based on specific
+            step, minimum quota and minimum difference between size and quota.
+
+        .EXAMPLE
+            Get-QuotaFromSize -gigaBytes 1.2
+
+            This returns 2.5.
+    #>
+    param(
+        [Double]$gigaBytes
+    )
+
+    [math]::Round($gigaBytes * [math]::Pow(2, 30))
+}
+
 foreach ($mailbox in $allMailboxes) {
     $mailboxSizeGigaBytes =
         Get-MailboxStatistics -Identity $mailbox | `
@@ -19,13 +75,12 @@ foreach ($mailbox in $allMailboxes) {
         } | `
         Select $sizeFieldName -ExpandProperty $sizeFieldName
 
-    if ($mailboxSizeGigaBytes -eq 0) {
-        $mailboxDesiredQuotaBytes = 2GB
-    } elseif ($mailboxSizeGigaBytes % 1 -lt 0.5 -and $mailboxSizeGigaBytes % 1 -gt 0) {
-        $mailboxDesiredQuotaBytes = ([math]::Round($mailboxSizeGigaBytes) + 1.5) * [math]::pow(2, 30)
-    } else {
-        $mailboxDesiredQuotaBytes = ([math]::Round($mailboxSizeGigaBytes) + 1) * [math]::pow(2, 30)
-    }
+    $mailboxDesiredQuotaGigaBytes = Get-QuotaForSize `
+        -size $mailboxSizeGigaBytes `
+        -minimumDifference $mailboxMinimumQuotaDifference `
+        -minimumQuota $mailboxMinimumQuota `
+        -step $mailboxQuotaStep
+    $mailboxDesiredQuotaBytes = Get-BytesFromGigaBytes -gigaBytes $mailboxDesiredQuotaGigaBytes
 
     $movingProhibitSendQuota = $mailboxDesiredQuotaBytes
     $movingIssueWarningQuota = [math]::Round($mailboxDesiredQuotaBytes * 0.9)
@@ -57,13 +112,12 @@ foreach ($mailbox in $allMailboxes) {
         $archiveSizeGigaBytes = 0
     }
 
-    if ($archiveSizeGigaBytes -eq 0) {
-        $archiveDesiredQuotaBytes = 5GB
-    } elseif ($archiveSizeGigaBytes % 1 -lt 0.5 -and $archiveSizeGigaBytes % 1 -gt 0) {
-        $archiveDesiredQuotaBytes = ([math]::Round($archiveSizeGigaBytes) + 5.5) * [math]::pow(2, 30)
-    } else {
-        $archiveDesiredQuotaBytes = ([math]::Round($archiveSizeGigaBytes) + 5) * [math]::pow(2, 30)
-    }
+    $archiveDesiredQuotaGigaBytes = Get-QuotaForSize `
+        -size $archiveSizeGigaBytes `
+        -minimumDifference $archiveMinimumQuotaDifference `
+        -minimumQuota $archiveMinimumQuota `
+        -step $archiveQuotaStep
+    $archiveDesiredQuotaBytes = Get-BytesFromGigaBytes -gigaBytes $archiveDesiredQuotaGigaBytes
 
     $movingArchiveQuota = $archiveDesiredQuotaBytes
     $movingArchiveWarningQuota = [math]::Round($archiveDesiredQuotaBytes * 0.9)
